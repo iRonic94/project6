6d10fcfcb897c255d539526fcdf40e9d7924e104
iRonic <mariocraciundaniel@gmail.com>
2026-04-17 02:12:53 +0300

    fixing a problem
    was causing communcation between frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 4000)
